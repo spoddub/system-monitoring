0f10481777663a4d63b6b4c8252fcf7cd2677d30
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-10-10 21:57:21 +0300

    add admin interface

---
+++ system_monitoring/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import Machine, MetricsSample
+
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "is_active", "jitter_sec", "updated_at")
+    list_filter = ("is_active",)
+    search_fields = ("name", "url")
+
+
+@admin.register(MetricsSample)
+class MetricsSampleAdmin:
+    list_display = (
+        "machine",
+        "collected_at",
+        "timeslot_star",
+        "cpu_pct",
+        "mem_pct",
+        "disk_pct",
+        "uptime_sec",
+    )
+    list_filter = ("machine",)
+    search_fields = ("machine__name",)
