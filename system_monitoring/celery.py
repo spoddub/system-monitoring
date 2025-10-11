db4c66b508c9196ba03839f8e4c74f47fd0265db
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-10-11 14:47:32 +0300

    add settings for running celery

--- system_monitoring/celery.py
+++ system_monitoring/celery.py
@@ -1,4 +1,7 @@
+import os
 from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "system_monitoring.settings")
 
 app = Celery("system_monitoring")
 # ищет все что начинается с CELERY
