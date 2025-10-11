5f0f49d7244a7bf95279d3456d9e455f933e182f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-10-12 01:16:15 +0300

    fix typo

--- system_monitoring/settings.py
+++ system_monitoring/settings.py
@@ -28,7 +28,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'system_monitoring.middleware.CustomAuthMiddleware'
+    'system_monitoring.middleware.CustomAuthMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
