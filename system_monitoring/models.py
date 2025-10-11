5c14c1dae7a9ba67b83db1b26ccb0f105895eed6
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-10-11 17:08:01 +0300

    add incident class

--- system_monitoring/models.py
+++ system_monitoring/models.py
@@ -39,3 +39,36 @@ class MetricsSample(models.Model):
 
     def __str__(self) -> str:
         return super().__str__()
+
+
+class Incident(models.Model):
+    class Type(models.TextChoices):
+        CPU = "cpu", "CPU > 85%"
+        MEM = "mem", "Memory > 90% (30m)"
+        DISK = "disk", "Disk > 95% (2h)"
+
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    # значения только из Type
+    type = models.CharField(max_length=10, choices=Type.choices)
+    active = models.BooleanField(default=True, db_index=True)
+
+    started_at = models.DateTimeField()  # начало инцидента
+    resolved_at = models.DateTimeField(null=True, blank=True)
+
+    first_timeslot = models.DateTimeField()  # в каком таймслоте началось
+    last_timeslot = models.DateTimeField()  # последний таймслот с инцидентом
+
+    details = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-started_at"]  # сначала новые
+        # mysql фильтр для поиска
+        indexes = [
+            models.Index(fields=["machine", "type", "active"]),
+        ]
+
+    def __str__(self) -> str:
+        incident_type = Incident.Type(self.type).label
+        status = "ACTIVE" if self.active else "SOLVED"
+        return f"Incident {incident_type} is {status}"
