369bb56c172a1821b759495633ce93d4fcafcad4
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-10-11 13:51:01 +0300

    add comment for understanding

--- system_monitoring/celery.py
+++ system_monitoring/celery.py
@@ -1,5 +1,6 @@
 from celery import Celery
 
 app = Celery("system_monitoring")
+# ищет все что начинается с CELERY
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
